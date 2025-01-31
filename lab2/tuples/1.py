9b0f2dcb3e4d933f9732619b896ffbc5d873974a
aibynkapsikh <aibynkapsikh77@gmail.com>
2025-02-01 01:35:42 +0500

    lab2

---
+++ lab2/tuples/1.py
@@ -0,0 +1,33 @@
+# Tuples
+
+my_tuple = ("banana", "applw", "cherry")
+
+# Tuples are used to store multiple items in a single variable.
+
+thistuple = ("apple", "banana", "cherry")
+print(thistuple)
+# Lenght tuple
+print(len(thistuple))
+
+# Create Tuple With One Item
+
+tuple_1 = ("apple",)
+
+print(type(tuple_1))
+
+
+#NOT a tuple
+tuple_2 = ("apple")
+print(type(tuple_2))
+
+# Access Tuple Items
+print(thistuple[1]) # Note: The first item has index 0. 
+
+# Range of Indexes
+this_tuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(this_tuple[2:5]) # Note: The search will start at index 2 (included) and end at index 5 (not included).
+
+print(thistuple[-4:-1]) # Specify negative indexes if you want to start the search from the end of the tuple:
+
+
+
